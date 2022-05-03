e46d7892b81040308a5b892e0d2bfb6cb487129c
James Harding <99635678+JaHa675@users.noreply.github.com>
2022-05-02 23:08:01 -0700

    fully functional

--- index.js
+++ index.js
@@ -152,7 +152,9 @@ function addRole() {
           if (err) {
             throw err;
           }
-          const departmentIdArray = res.filter(word => word.name === answer.roleDept);
+          const departmentIdArray = res.filter(
+            (word) => word.name === answer.roleDept
+          );
           departmentId = departmentIdArray[0].id;
         });
         const insertRoleSql = `INSERT INTO roles (title, salary, department) VALUES (?, ?, ?)`;
@@ -168,8 +170,159 @@ function addRole() {
   });
 }
 
-function addEmployee() {}
+function addEmployee() {
+  const sql = "SELECT roles.title FROM roles";
+  db.query(sql, (err, res) => {
+    if (err) {
+      throw err;
+    }
+    const roles = res.map((role) => role.title);
 
-function updateEmployee() {}
+    const managersql =
+      "SELECT CONCAT(first_name,' ', last_name) AS name FROM employee";
+    db.query(managersql, (err, res) => {
+      if (err) {
+        throw err;
+      }
+      const employees = res.map((name) => name.name);
+      console.log(employees);
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "firstName",
+            message: "First Name?",
+          },
+          {
+            type: "input",
+            name: "lastName",
+            message: "Last name?",
+          },
+          {
+            type: "list",
+            name: "newRole",
+            message: "What role?",
+            choices: [...roles],
+          },
+          {
+            type: "list",
+            name: "managerName",
+            message: "Managers name?",
+            choices: [...employees],
+          },
+        ])
+        .then((answer) => {
+          db.query(
+            `SELECT CONCAT(first_name,' ', last_name) AS name, id FROM employee`,
+            (err, res) => {
+              if (err) {
+                throw err;
+              }
+              const selectManagerArray = res.filter(
+                (word) => word.name === answer.managerName
+              );
+              const managerId = selectManagerArray[0].id;
+              db.query(`SELECT * FROM roles`, (err, res) => {
+                if (err) {
+                  throw err;
+                }
+                const selectRoleArray = res.filter(
+                  (word) => word.title === answer.newRole
+                );
+                console.log(selectRoleArray)
+                const selectRoleId = selectRoleArray[0].id;
+                const insertEmployeeSql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+                const params = [
+                  answer.firstName,
+                  answer.lastName,
+                  selectRoleId,
+                  managerId,
+                ];
+                db.query(insertEmployeeSql, params, (err, res) => {
+                  if (err) {
+                    throw err;
+                  }
+                  ask();
+                });
+              });
+            }
+          );
+        });
+    });
+  });
+}
+
+function updateEmployee() {
+  const sql =
+    "SELECT title, roles.id FROM roles INNER JOIN department ON roles.department = department.id";
+  db.query(sql, (err, res) => {
+    if (err) {
+      throw err;
+    }
+    const rolesList = res.map((role) => role.title);
+    db.query(
+      `SELECT CONCAT(first_name,' ', last_name) AS employee_name FROM employee;`,
+      (err, res) => {
+        if (err) {
+          throw err;
+        }
+        const namesList = res.map((empName) => empName.employee_name);
+        console.log(namesList);
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              name: "chosenEmployee",
+              message: "Which employee would you like to update?",
+              choices: namesList,
+            },
+            {
+              type: "list",
+              name: "chosenRole",
+              message: "What role do they have now?",
+              choices: rolesList,
+            },
+          ])
+          .then((answer) => {
+            console.log(answer);
+            let roleId;
+            let employeeId;
+            db.query(
+              "SELECT CONCAT(first_name,' ', last_name) AS employee_name, employee.id FROM employee",
+              (err, res) => {
+                if (err) {
+                  throw err;
+                }
+                const employeeIdArray = res.filter(
+                  (word) => word.employee_name === answer.chosenEmployee
+                );
+                employeeId = employeeIdArray[0].id;
+
+                db.query("SELECT * FROM roles", (err, res) => {
+                  if (err) {
+                    throw err;
+                  }
+                  const roleIdArray = res.filter(
+                    (word) => word.name === answer.chosenrole
+                  );
+                  roleId = roleIdArray[0].id;
+                  db.query(
+                    `UPDATE employee SET role_id = ${roleId} WHERE id = ${employeeId};`,
+                    (err, res) => {
+                      if (err) {
+                        throw err;
+                      }
+                      console.log("update successful");
+                      ask();
+                    }
+                  );
+                });
+              }
+            );
+          });
+      }
+    );
+  });
+}
 
 ask();
